d0f7c7fad8143de375b828b545fdb79ed4abcdca
Volodymyr Dorosh <38923744+sQverful@users.noreply.github.com>
2021-02-19 14:46:55 +0200

    Add files via upload

---
+++ includes/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$connection = mysqli_connect('127.0.0.1', 'root', '', 'restaurant');
+
+if( $connection == false)
+{
+	echo 'Не вдалось підключитись до бази даних!<br>';
+	echo mysqli_connect_error();
+	exit();
+} 
+?>
